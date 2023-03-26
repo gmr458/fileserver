411403a9bb4cca238531d6a97cea20d8bf536691
German David <germanmarinrolong@gmail.com>
2023-03-26 15:18:00 -0500

    update

--- main.go
+++ main.go
@@ -1,25 +1,108 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+)
+
+var (
+	staticDirName string
+	port          int
 )
 
 func main() {
-	const nameDir = "static"
-	err := os.Mkdir(nameDir, os.ModePerm)
+	flag.IntVar(&port, "port", 4000, "Server port")
+	flag.StringVar(&staticDirName, "dir", "", "Directory to expose")
+	flag.Parse()
+	if staticDirName == "" {
+		fmt.Println("Pass the -dir flag")
+		os.Exit(1)
+	}
 
-	if err != nil && err.Error() != fmt.Sprintf("mkdir %s: file exists", nameDir) {
+	fs := http.FileServer(http.Dir(staticDirName))
+	http.Handle("/", fs)
+	http.HandleFunc("/downloadall", handlerDownloadAll)
+
+	fmt.Printf("Local: http://localhost:%d/\n", port)
+
+	ips := getIps()
+	networkLinks := ""
+	for k, ip := range ips {
+		networkLinks += fmt.Sprintf("http://%s:%d/", ip, port)
+
+		if k == len(ips)-1 {
+			break
+		}
+
+		networkLinks += ", "
+	}
+	fmt.Printf("Network: %s\n", networkLinks)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func getIps() []string {
+	interfaces, err := net.Interfaces()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	var ips []string
 
-	const ADDR = ":3000"
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	log.Printf("listening on http://localhost%s", ADDR)
-	log.Fatal(http.ListenAndServe(ADDR, nil))
+		for k, a := range addrs {
+			if k == 0 {
+				if !strings.HasPrefix(a.String(), "127") {
+					ip := strings.Split(a.String(), "/")[0]
+					ips = append(ips, ip)
+				}
+			}
+		}
+	}
+
+	return ips
+}
+
+func handlerDownloadAll(w http.ResponseWriter, r *http.Request) {
+	dirEntries, err := os.ReadDir(staticDirName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	ips := getIps()
+
+	var text string
+
+	for _, ip := range ips {
+		text += "--------------------------------------\n"
+		text += fmt.Sprintf("Links available for IP %s\n\n", ip)
+
+		cmd := "wget"
+		for k, v := range dirEntries {
+			filename := v.Name()
+			cmd += fmt.Sprintf(" \"http://%s:%d/%s\"", ip, port, filename)
+
+			if k == len(dirEntries)-1 {
+				break
+			}
+
+			cmd += " \\\n"
+		}
+
+		text += cmd
+	}
+
+	_, err = w.Write([]byte(text))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
